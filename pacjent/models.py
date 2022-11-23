7fbd0741a415fde3803be02d5608d98959433374
polishfrog <dawidrozewski21@gmail.com>
2022-11-23 21:31:37 +0100

    Create simple models and configure projet

--- pacjent/models.py
+++ pacjent/models.py
@@ -1,3 +1,46 @@
+import datetime
+
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+def check_post_code(value):
+    pass
+
+
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    pesel = models.CharField(max_length=11)
+    date_of_birth = models.DateField()
+    street = models.CharField(max_length=128)
+    build_number = models.CharField(max_length=16)
+    apartment_number = models.CharField(max_length=16,
+                                        null=True,
+                                        default=None)
+    post_code = models.CharField(max_length=6, validators=[check_post_code])
+    city = models.CharField(max_length=128)
+
+
+class Place(models.Model):
+    pass
+
+
+class Special(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class Doctor(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    special = models.OneToOneField(Special, on_delete=models.CASCADE)
+    date_of_employment = models.DateField(default=datetime.date)
+
+
+class DoctorInPlace(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+
+
+
--- pacjentapp/settings.py
+++ pacjentapp/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'pacjentapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'HOST': '127.0.0.1',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pacjentappdb',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab'
     }
 }
 
@@ -116,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = STATIC_URL + '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
